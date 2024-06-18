9ed0609769802752b46c1d02f59e7e5fefc377d0
Claudio-Sjo <Claudio.Porfiri@gmail.com>
2024-06-18 12:47:04 +0200

    Fixing...

--- rtlsdr_ft8d.cpp
+++ rtlsdr_ft8d.cpp
@@ -1017,7 +1017,7 @@ void decode(const monitor_t *mon, struct tm *tm_slot_start, struct decoder_resul
 
                 found_duplicate = true;
             } else {
-                LOG(LOG_DEBUG, "Decoded Hash table clash!\n");
+                LOG(LOG_DEBUG, "Hash table clash!\n");
                 // wprintw(logwL, "Hash table clash!\n");
 
                 // Move on to check the next entry in hash table
@@ -1025,8 +1025,7 @@ void decode(const monitor_t *mon, struct tm *tm_slot_start, struct decoder_resul
                     {
                         LOG(LOG_DEBUG, "Decoded Hash table overridden!\n");
                         decoded_hashtable[0] = NULL;
-                    }                    
-                }
+                    }            
                 idx_hash = (idx_hash + 1) % K_MAX_MESSAGES;
             }
             // wrefresh(logwL);
