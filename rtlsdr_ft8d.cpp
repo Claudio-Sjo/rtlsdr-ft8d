4a9f975b5e31d190bc0abd24aeb2bbf778e294e7
Claudio-Sjo <Claudio.Porfiri@gmail.com>
2024-06-20 10:34:34 +0200

    Hastable removed

--- rtlsdr_ft8d.cpp
+++ rtlsdr_ft8d.cpp
@@ -845,103 +845,24 @@ int32_t decoderSelfTest() {
 }
 
 
-#define CALLSIGN_HASHTABLE_SIZE 256
-
-static struct
-{
-    char callsign[12]; ///> Up to 11 symbols of callsign + trailing zeros (always filled)
-    uint32_t hash;     ///> 8 MSBs contain the age of callsign; 22 LSBs contain hash value
-} callsign_hashtable[CALLSIGN_HASHTABLE_SIZE];
-
-static int callsign_hashtable_size;
-
 void hashtable_init(void)
 {
-    callsign_hashtable_size = 0;
-    memset(callsign_hashtable, 0, sizeof(callsign_hashtable));
+
 }
 
 void hashtable_cleanup(uint8_t max_age)
 {
-    for (int idx_hash = 0; idx_hash < CALLSIGN_HASHTABLE_SIZE; ++idx_hash)
-    {
-        if (callsign_hashtable[idx_hash].callsign[0] != '\0')
-        {
-            uint8_t age = (uint8_t)(callsign_hashtable[idx_hash].hash >> 24);
-            if (age > max_age)
-            {
-                LOG(LOG_INFO, "Removing [%s] from hash table, age = %d\n", callsign_hashtable[idx_hash].callsign, age);
-                // free the hash entry
-                callsign_hashtable[idx_hash].callsign[0] = '\0';
-                callsign_hashtable[idx_hash].hash = 0;
-                callsign_hashtable_size--;
-            }
-            else
-            {
-                // increase callsign age
-                callsign_hashtable[idx_hash].hash = (((uint32_t)age + 1u) << 24) | (callsign_hashtable[idx_hash].hash & 0x3FFFFFu);
-            }
-        }
-    }
+
 }
 
 void hashtable_add(const char* callsign, uint32_t hash)
 {
-
     // This doesn't work, let's move it out for now
     return;
-    // End of fix
-    
-    uint16_t hash10 = (hash >> 12) & 0x3FFu;
-    int idx_hash = (hash10 * 23) % CALLSIGN_HASHTABLE_SIZE;
-    int old_idx = idx_hash;
-    bool overridden = false;
-    while (callsign_hashtable[idx_hash].callsign[0] != '\0')
-    {
-        if (((callsign_hashtable[idx_hash].hash & 0x3FFFFFu) == hash) && (0 == strcmp(callsign_hashtable[idx_hash].callsign, callsign)))
-        {
-            // reset age
-            callsign_hashtable[idx_hash].hash &= 0x3FFFFFu;
-            LOG(LOG_DEBUG, "ADD Found a duplicate [%s]\n", callsign);
-            return;
-        }
-        else
-        {
-            LOG(LOG_DEBUG, "ADD Hash table clash!\n");
-            // Move on to check the next entry in hash table
-            if (idx_hash + 1 == old_idx) // Wrapped all the table
-                {
-                LOG(LOG_DEBUG, "ADD Hash table overridden!\n");
-                callsign_hashtable[old_idx].callsign[0] = '\0';
-                callsign_hashtable[old_idx].hash = 0;
-                overridden = true;
-                }
-            idx_hash = (idx_hash + 1) % CALLSIGN_HASHTABLE_SIZE;
-        }
-    }
-    if (!overridden)
-        callsign_hashtable_size++;
-    strncpy(callsign_hashtable[idx_hash].callsign, callsign, 11);
-    callsign_hashtable[idx_hash].callsign[11] = '\0';
-    callsign_hashtable[idx_hash].hash = hash;
 }
 
 bool hashtable_lookup(ftx_callsign_hash_type_t hash_type, uint32_t hash, char* callsign)
 {
-    uint8_t hash_shift = (hash_type == FTX_CALLSIGN_HASH_10_BITS) ? 12 : (hash_type == FTX_CALLSIGN_HASH_12_BITS ? 10 : 0);
-    uint16_t hash10 = (hash >> (12 - hash_shift)) & 0x3FFu;
-    int idx_hash = (hash10 * 23) % CALLSIGN_HASHTABLE_SIZE;
-    while (callsign_hashtable[idx_hash].callsign[0] != '\0')
-    {
-        if (((callsign_hashtable[idx_hash].hash & 0x3FFFFFu) >> hash_shift) == hash)
-        {
-            strcpy(callsign, callsign_hashtable[idx_hash].callsign);
-            return true;
-        }
-        // Move on to check the next entry in hash table
-        idx_hash = (idx_hash + 1) % CALLSIGN_HASHTABLE_SIZE;
-    }
-    callsign[0] = '\0';
     return false;
 }
 
